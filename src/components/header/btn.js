a8848168dea3377c917bec1f1c9f388bb18c3e0f
gndhstr <ghandisatria@gmail.com>
2024-06-18 22:44:32 +0700

    update download button

--- src/components/header/btn.js
+++ src/components/header/btn.js
@@ -3,7 +3,7 @@ import React from 'react'
 const btn = () => {
   return (
         <div className='btn__header'>
-            <a href="/GandhiSatria-resume.pdf" target="_blank" className='btn'>View CV</a>
+            <a href="/GandhiSatria-resume.pdf" download className='btn'>Download CV</a>
             <a href="#about" className='btn btn-primary'>Know Me</a>
         </div>
   )
